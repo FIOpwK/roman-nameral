9ca82e761c02762739c5739c72025679a2c85bc1
Dajuan Mcdonald <flopwiki@gmail.com>
2020-03-27 18:42:23 -0400

    moved object to own component

--- roman-nameral/src/components/Buttons.js
+++ roman-nameral/src/components/Buttons.js
@@ -1,20 +1,8 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react';
 import Media from 'react-media';
+import obj from '../obj';
 
-const obj = {
-    X: 10,
-    M: 1000,
-    I: 1,
-    C: 100,
-    D: 500,
-    L: 50,
-    V: 5,
-    Y: 5000,
-    Q: 10,
-    R: 1,
-    S: 5
-}
 
 export default function Buttons() {
     const [total, setTotal] = React.useState(0);
---
+++ roman-nameral/src/obj.js
@@ -0,0 +1,15 @@
+const obj = {
+    X: 10,
+    M: 1000,
+    I: 1,
+    C: 100,
+    D: 500,
+    L: 50,
+    V: 5,
+    Y: 5000,
+    Q: 10,
+    R: 1,
+    S: 5
+}
+
+export default obj;
