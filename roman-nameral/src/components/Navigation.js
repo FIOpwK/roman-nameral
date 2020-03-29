fcc6d1c9f0447b8889c2f97e3fad65fea174335b
Dajuan Mcdonald <flopwiki@gmail.com>
2020-03-29 13:20:01 -0400

    fixed light-mode dark-mode toggling

--- roman-nameral/src/components/Navigation.js
+++ roman-nameral/src/components/Navigation.js
@@ -2,25 +2,37 @@ import React from 'react';
 import Switch from '@material-ui/core/Switch';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import useLocalStorage from '../hooks/useLocalStorage';
 
 
-export default function Navigation(props) {
-    const [darkMode, setDarkmode] = React.useState(false);
 
+const Navigation = (e) => {
+    const [darkMode, setDarkmode] = React.useState(true);
+
+    
     const handleChange = e => {
         e.preventDefault();
         setDarkmode(!darkMode)
     }
-
+    
     return (
         <div>
             <FormGroup>
                 <FormControlLabel
                     control={<Switch checked={darkMode} onChange={handleChange} />}
                     className={darkMode ? 'dark-mode' : 'light-mode'}
-                />
+                    />
             </FormGroup>
+
+            {darkMode ? document.body.classList.add('dark-mode') : 
+            document.body.classList.remove('dark-mode') },
+
+           {!darkMode ? document.body.classList.add('light-mode') :
+            document.body.classList.remove('light-mode')}
+            
 
         </div>
     )
 }
+
+export default Navigation;
