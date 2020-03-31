f6b6067fc0f6ef37fb9df40eca0c350bb28f6c3c
Dajuan Mcdonald <flopwiki@gmail.com>
2020-03-30 23:33:14 -0400

    added share to master

--- roman-nameral/src/App.js
+++ roman-nameral/src/App.js
@@ -13,7 +13,7 @@ const App = () => {
       <TextItems/>
 
       <Buttons/>
-
+      <div class="fb-share-button" data-href="https://roman-nameral-ii.now.sh/" data-layout="button_count" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Froman-nameral-ii.now.sh%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share to facebook</a></div>
 
     </div>
   );
